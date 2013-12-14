4ee9c2206ff1d0c983eea6e21d1d48f274545333
Nicholas Sarkis <gcflames5@gmail.com>
2013-12-13 22:23:47 -0500

    Add TeamColor Enum

---
+++ src/net/njay/njaypi/Team/TeamColor.java
@@ -0,0 +1,21 @@
+package net.njay.njaypi.Team;
+
+public enum TeamColor {
+	RED("Red"), BLUE("Blue"), GREEN("Green"), GOLD("Gold"), PURPLE("Purple");
+	
+	String name;
+	TeamColor(String name){
+		this.name = name;
+	}
+	public String getName(){
+		return this.name;
+	}
+	
+	public static TeamColor parse(String s){
+		for (TeamColor color : TeamColor.values()){
+			if (color.getName().equalsIgnoreCase(s))
+				return color;
+		}
+		return null;
+	}
+}
